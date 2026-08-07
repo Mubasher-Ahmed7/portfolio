52cb6899ed9b593aebdfc2e56aa8adde54e9b353
Mubasher Ahmed <mubasher@local>
2026-08-07 18:13:29 +0500

    Add Firebase project config

--- projects/firebase-app/src/firebase/config.js
+++ projects/firebase-app/src/firebase/config.js
@@ -1,23 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD0ZLTmSql2KUBz3g0OrvJ9wmPJ6KBDyn8",
-  authDomain: "portfolio-app-132be.firebaseapp.com",
-  projectId: "portfolio-app-132be",
-  storageBucket: "portfolio-app-132be.firebasestorage.app",
-  messagingSenderId: "685452574109",
-  appId: "1:685452574109:web:cbe6ab046a7bce447f4374",
-  measurementId: "G-1K5X5RKK0H"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  apiKey: 'AIzaSyD0ZLTmSql2KUBz3g0OrvJ9wmPJ6KBDyn8',
+  authDomain: 'portfolio-app-132be.firebaseapp.com',
+  projectId: 'portfolio-app-132be',
+  storageBucket: 'portfolio-app-132be.firebasestorage.app',
+  messagingSenderId: '685452574109',
+  appId: '1:685452574109:web:cbe6ab046a7bce447f4374',
+  measurementId: 'G-1K5X5RKK0H',
+}
 
 export default firebaseConfig
